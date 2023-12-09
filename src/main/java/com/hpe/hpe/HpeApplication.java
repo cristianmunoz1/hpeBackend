310002fe8ea4dd927bd211169655ce7b58ed9b49
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-12-08 21:37:51 -0500

    Se crea el proyecto y se agrega Spring Security

---
+++ src/main/java/com/hpe/hpe/HpeApplication.java
@@ -0,0 +1,13 @@
+package com.hpe.hpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpeApplication.class, args);
+	}
+
+}
